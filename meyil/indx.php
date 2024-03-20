3de13d7300ac866e7355c1a422c0c516fd00baf3
cennetkara <155713948+cennetkara@users.noreply.github.com>
2024-03-20 11:59:30 +0300

    Add files via upload
    } else {
        $response["success"] = false;
        $response["message"] = "Dosya yüklenirken bir hata oluştu.";
    }
    
    // JSON çıktısını göndermeden önce herhangi bir çıktı olmadığından emin olun
    ob_clean();

---
+++ meyil/indx.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="#">
+            <i class="url-icon uil uil-link"></i>
+            <input type="text" placeholder="mesajınızı yazın">
+            <button>Tıkla</button>
+        </form>
+        <div class="count"></div>
+        <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Toplam bağlantılar:<span> 10</span> & Toplam Tıklama<span> 140</span></span>
+   
+        <div class="urls-area">
+        <div class="title">
+            <li>&nbsp;&nbsp;&nbsp;URL'yi kısalt</li>
+            <li>&nbsp;&nbsp;&nbsp;orijinal URL</li>
+            <li>&nbsp;&nbsp;&nbsp;Tıklama</li>
+            <li>&nbsp;&nbsp;&nbsp;Aksiyon</li>
+        </div>
+    </div>
+    <div class="urls-area">
+        <div class="title">
+            <li>&nbsp;&nbsp;&nbsp;URL'yi kısalt</li>
+            <li>&nbsp;&nbsp;&nbsp;orijinal URL</li>
+            <li>&nbsp;&nbsp;&nbsp;Tıklama</li>
+            <li>&nbsp;&nbsp;&nbsp;Aksiyon</li>
+        </div>
+    </div> </div>
+   
+</body>
+</html>
